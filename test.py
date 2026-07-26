6ddf98ce844bc984989bf9ded8491231bbb98620
huwoha <3375553289qq.com>
2026-07-26 11:50:48 +0800

    modified:   test.py

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("hellow")
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello():
+    return jsonify({"message": "Hello, World!"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
