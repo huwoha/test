bc258cd64a17a5c66993fa0e8e323265a5acb9fc
huwoha <3375553289qq.com>
2026-08-05 21:29:48 +0800

    modified:   .vscode/tasks.json 	modified:   1.exe 	new file:   1_v1.1.zip 	new file:   1_v1.1/1.exe

--- 1.cpp
+++ 1.cpp
@@ -1,69 +1,60 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    // cout << "Hello, World!" << endl;
-    // int a;
-    // a = 10;
-    // cout << a << endl;
-    // a = 20;
-    // cout << a << endl;
-    // int a = 10; //初始化
-    // float b = 3.14f;
-    // double c = 1.2345;
-    // char d = 'f';
-    // bool e = 123;
-    // cout << a << "\t" << b << "\t" << c << "\t" << d << "\t" << e << endl;
-
-    // int a = 10;
-    // int b = 20;
-    // int c = a + b;
-    // const int d = 100;
-    // int e = c + d;
-
-    // cout << e << endl;
-    // int a;
-    // cin >> a;
-    // int b = 10;
-    // int c = a * b;
-    // cout << c << endl;
-    // int _a;
-    // int $12;
-    // int a1;
-    // int A;
-    // int A$a;
-    // int _;
-
-    // int aaa;
-    // float a = 10 / 3.0;
-    // cout << e << endl;
-    // bool I = 1>=1;
-    // cout << I << endl;
-    // bool I = !1;
-    // cout << I << endl;
-    // int i = 10;
-    // int I = i ^ 2;
-    // cout << I << endl;
-    cout << "请输入两个整数" << endl;
-    cout <<"这个数是两个5的和"<< endl;
-    int a;
-    int b;
-    cin >> a;
-    cin >> b;
-    int c =a+b;
-    if (c>10)
+    cout << "这是一个猜数字游戏，请输入两个1-100之间的数字" << endl;
+    cout << "让它们的和等于你要猜的数字" << endl;
+    cout << "请输入'yes'开始游戏" << endl;
+    string l;
+    cin >> l;
+    if (l == "yes")
     {
-        cout << "你找到答案了" << endl;
+        cout << "请输入两个整数" << endl;
+        cout << "这个数是两个5的和" << endl;
+        int a;
+        cin >> a;
+        int b;
+        cin >> b;
+        int c = a + b;
+        switch (c)
+        {
+        case 10:
+            cout << "你找到答案了" << endl;
+            break;
+        default:
+            cout << "你没有找到答案,还有2次机会" << endl;
+            int d;
+            cin >> d;
+            int e;
+            cin >> e;
+            int f = d + e;
+            switch (f)
+            {
+            case 10:
+                cout << "你找到答案了" << endl;
+                break;
+            default:
+                cout << "你没有找到答案,还有1次机会" << endl;
+                int g;
+                cin >> g;
+                int h;
+                cin >> h;
+                int i = g + h;
+                switch (i)
+                {
+                case 10:
+                    cout << "你找到答案了" << endl;
+                    break;
+                default:
+                    cout << "你没有找到答案，你输了" << endl;
+                    break;
+                }
+            }
+            break;
+        }
     }
-    else
-    {
-        cout << "你没有找到答案" << endl;
-    }
-
-
-
-
     system("pause");
     return 0;
 }
