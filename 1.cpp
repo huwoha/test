407c6d9c002f9a1f30d2decdb5ed166fe3e28c11
huwoha <3375553289qq.com>
2026-08-05 21:19:26 +0800

    new file:   .vscode/c_cpp_properties.json 	new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	new file:   1.cpp 	new file:   1.exe 	new file:   123.py

---
+++ 1.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cout << "Hello, World!" << endl;
+    // int a;
+    // a = 10;
+    // cout << a << endl;
+    // a = 20;
+    // cout << a << endl;
+    // int a = 10; //初始化
+    // float b = 3.14f;
+    // double c = 1.2345;
+    // char d = 'f';
+    // bool e = 123;
+    // cout << a << "\t" << b << "\t" << c << "\t" << d << "\t" << e << endl;
+
+    // int a = 10;
+    // int b = 20;
+    // int c = a + b;
+    // const int d = 100;
+    // int e = c + d;
+
+    // cout << e << endl;
+    // int a;
+    // cin >> a;
+    // int b = 10;
+    // int c = a * b;
+    // cout << c << endl;
+    // int _a;
+    // int $12;
+    // int a1;
+    // int A;
+    // int A$a;
+    // int _;
+
+    // int aaa;
+    // float a = 10 / 3.0;
+    // cout << e << endl;
+    // bool I = 1>=1;
+    // cout << I << endl;
+    // bool I = !1;
+    // cout << I << endl;
+    // int i = 10;
+    // int I = i ^ 2;
+    // cout << I << endl;
+    cout << "请输入两个整数" << endl;
+    cout <<"这个数是两个5的和"<< endl;
+    int a;
+    int b;
+    cin >> a;
+    cin >> b;
+    int c =a+b;
+    if (c>10)
+    {
+        cout << "你找到答案了" << endl;
+    }
+    else
+    {
+        cout << "你没有找到答案" << endl;
+    }
+
+
+
+
+    system("pause");
+    return 0;
+}
